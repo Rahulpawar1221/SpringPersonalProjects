7fc2058be84a6a5d7e5985795da8b094ec18a02d
Your Name <your.email@example.com>
2025-10-27 10:31:46 +0530

    JWT Project Added

---
+++ LearnJWT/src/main/java/com/JWT/LearnJWT/Repository/UserDetailsRepository.java
@@ -0,0 +1,14 @@
+package com.JWT.LearnJWT.Repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.JWT.LearnJWT.Entity.Users;
+
+@Repository
+public interface UserDetailsRepository extends JpaRepository<Users, Long>{
+
+	Optional<Users> findByUsername(String username);
+}
